e58d083688603c63a9696fcfdc0a0bb05e90ca53
osalusan <osalusan.ok@gmail.com>
2025-05-16 01:34:06 +0900

    shadowのコメントを修正

--- shadow/ShadowVolume.h
+++ shadow/ShadowVolume.h
@@ -8,6 +8,7 @@ enum class ANIMETION_MODEL;
 class StaticMeshObject;
 class Character;
 class MeshFiled;
+// ステンシルシャドウを使うクラス
 class ShadowVolume final:public GameObject
 {
 protected:
@@ -15,14 +16,14 @@ protected:
 	STATICMESH_MODEL m_StaticModel;						
 	ANIMETION_MODEL m_AnimeModel;						
 
-	StaticMeshObject* m_StaticMeshCache = nullptr;
-	Character* m_CharacterCache = nullptr;
+	StaticMeshObject* m_StaticMeshCache = nullptr;		// 影として使用
+	Character* m_CharacterCache = nullptr;				// モデルのアニメーションを反映させるように / ステンシルシャドウだと上手く影を表現できないので、違う影を実装した方がいい
 
-	MeshFiled* m_MeshFiled = nullptr;
+	MeshFiled* m_MeshFiled = nullptr;					// 影を地面の高さに描画
 public:
 	ShadowVolume() = delete;
-	ShadowVolume(StaticMeshObject* staticMesh);
-	ShadowVolume(Character* character);
+	ShadowVolume(StaticMeshObject* staticMesh);			// 影として描画したいStaticMeshを引数に
+	ShadowVolume(Character* character);					// 影として描画したいCharacterを引数に
 	virtual void Init()override;
 	virtual void Update(const float& deltaTime)override;
 	virtual void Draw()override;
